7b15db14298b983b39b99ce80e677e08b609deeb
onlyrevolutions57@gmail.com <onlyrevolutions57@gmail.com>
2023-02-19 12:18:08 -0800

    Finished array Method notes

--- Unit_3/arrayMethods.js
+++ Unit_3/arrayMethods.js
@@ -1,38 +1,35 @@
 // ? Array Methods, awesome ways to do things with or to arrays
 
+let food = ["Pecan Pie", "Shrimp", "Quesadilla", "Hotdog"];
 
-let food = ["Pecan Pie", "Shrimp", "Quesadilla", "Hot Dog"];
+// for(item of food){
+//     console.log(item);
+// }
 
-for (item of food){
-    console.log(item);
-}
-
-// * .push(), adds item to the end of an array
+// * .push(), adds item(s) to the end of an array
 
 food.push("Pizza", "More Pie");
-console.log("Push method:", food); // Push method: ["Pecan Pie", "Shrimp", "Quesadilla", "Hot Dog", "Pizza", "More Pie"]
+console.log("Push method: ", food); // Push method:  [ 'Pecan Pie', 'Shrimp', 'Quesadilla', 'Hotdog', 'Pizza', 'More Pie' ]
 // console.log(food[4]); // Pizza
 
-// * .splice(), splice(indices of where to remove how many to remove, what to replace with/add)
+// * .splice(), splice(indices of where to remove , how many to remove, what to replace with/add)
 food.splice(1, 1, "Tacos");
-console.log("Splice method:", food);
+console.log("Splice method: ", food);
 
 food.splice(1, 0, "Steak");
-console.log("Splice method:", food);
+console.log("Second Splice method: ", food);
 
-// * .pop() removes value from end of array
+// * .pop(), removes value from end of array
 food.pop();
-console.log("Pop method:", food);
+console.log("Pop method: ", food);
 
-// * .shift(), removes the first value of an array (value in first position)
-
+// * .shift(), removes the first value of an array(value in first position)
 food.shift();
-
-console.log("SHift method:", food);
+console.log("Shift method: ", food);
 
 // * .unshift(), adds to the front of an array, can include multiple items
 food.unshift("cake", "sushi", "roti");
-console.log("Unshift method:", food);
+console.log("Unshift method: ", food);
 
 // .length, property not method
 console.log(food.length);
@@ -41,51 +38,190 @@ console.log(food.length);
 let rgb = ["red", "green", "blue"];
 console.log("ToString Method: ", rgb.toString());
 
-
-// ? ITERATION METHODS
-
+//? Iteration Methods
 /* 
-    ? .filter() & .includes(
-        - does not destroy the original array
-        -we typically store the filter array in a new variable
-        -takes a callback function that returns either true or false
-        - A callback function is another function that we invoke within our main function (nested function)
-        -loops through array
-        -Must have a return
-    
+    ? .filter() & .includes()
+    - does not destroy the original array
+    - we typically store the filter array in a new variable
+    - takes a callback function that returns either true or false
+        - A callback function is another function that we invoke within our main function(nested function)
+    - loops through array
+    - MUST have a return
 */
 
-let fruit = ["Kumquat", "Banana", "Peach", "Kiwi", "Papaya", "Mango"];
+let fruits = ["Kumquat", "Banana", "Peach", "Kiwi", "Papaya", "Mango"];
 
 // Use filter to remove peach
 
-/* let filteredFruit = fruits.filter(fruit  => {   
-    let remainingFruit = fruit !== "Peach";
-    console.log(`Remaining fruit: ${remainingFruit}`);
-    return remainingFruit;
-})
+// let filteredFruit = fruits.filter((item) => {
+//   let remainingFruit = item !== "Peach";
+//   console.log(`Remaining fruit: ${remainingFruit}`);
+//   return remainingFruit;
+// });
 
-console.log(filteredFruit); */
+// console.log(filteredFruit);
 
-// .includes(), remove any fruit with "p"
-let otherfruit = "Pineapple";
-//console.log("Includes method:" , fruit.includes("p")); //false .includes done on one item at a time (single value at a time)
-console.log("Includes method:" , otherfruit.includes("p"));
-console.log("Includes method:" , otherfruit.includes("apple"));//true
+// .includes()
+let otherFruit = "Pineapple";
+// console.log("Includes method:", fruits.includes("P")); // false, .includes done on one item at a time (single value at a time)
+console.log("Includes method:", otherFruit.includes("p")); // true
+console.log("Includes method:", otherFruit.includes("apple")); // true
 
-// .filter with .includes , remove any fruit with "P"
+// .filter with .includes, remove any fruit with "P"
 
-const filterFruit2 = fruit.filter((item) => !item.includes("P"));
+const filterFruit2 = fruits.filter((item) => !item.includes("P"));
 console.log(filterFruit2);
-// set the filtered array to variable, filter will run eatMango function per item in array
 
-const filteredFruitFunctionEx = fruit.filter(eatMango);
+// set the filtered array to variable, filter will run eatMango function per item in array
+const filteredFruitFunctionEx = fruits.filter(eatMango);
+
 // returns true if not mango, false if mango
 function eatMango(fruit) {
-    return fruit !== "Mango";
+  return fruit !== "Mango";
 }
+
+// All that ^^^ is the same as:
+// const filteredFruitFunctionEx = fruits.filter((fruit) => {
+//     return fruit !== "Mango";
+//   });
+
 // printing to see final filtered array
+console.log("Filter with function ex: ", filteredFruitFunctionEx);
 
-console.log("Filter with function ex: ", filteredFruitFunctionEx )
+// console.log(eatMango("Baseball")) // demo function
 
-//console.log(eatMango("baseball"))// demo function 
+// ------------------------------------------------------------
+
+
+//? .forEach()
+/* 
+  runs 3 arguments:
+  - The Value
+  - The index
+  - The array object itself
+
+  - forEach() helps us iterate an array performing a function once per element in the array via a callback function.
+  - Is not executed for empty elements
+  -Does not change the original array
+    
+*/
+
+let newFoodList = ["apple", "pear", "mushrooms", "cheese", "spagett"];
+
+// for (let i = 0; i < newFoodList.length; i++) {
+// console.log(newFoodList[i]);
+// }
+
+// newFoodList.forEach((item) => console.log ("ForEach Method:", item));
+//ARROW FUNCTION
+newFoodList.forEach((item, i) =>  {
+  console.log("ForEach with Index:", item, i);
+})
+  //DELARATIVE FUNCTION
+newFoodList.forEach(function (item) {
+  console.log("Declaration sample:", item);
+})
+
+//Build a function to console.log item
+//Separating function from forEach, works
+function displayFoodItem (foodThing) {
+  console.log(`Function outside of forEach: ${foodThing}.`)
+}
+//function call
+//displayFoodItem("potato")
+
+//Pass our forEach method the function
+newFoodList.forEach(displayFoodItem);
+
+//-------------------------------------------
+//? .map()
+
+/* 
+  - Creates a new array from calling a function for every array element
+  -Calls the function once per element in an array
+  -Does not execute the function for empty elements
+  -Does not change the original array
+*/
+
+
+// Populated by for loop
+let numArray = [];
+// Populate by using .map on numArray
+let fizzBuzzArray = [];
+
+for (let k = 0; k <= 30; k++ ) {
+    numArray.push(k)
+}
+// Checking if numArray was populated
+//console.log("NumArray:",numArray)
+
+
+// Simple ex first
+//numArray.map((x) => fizzBuzzArray.push(x + 5));
+
+// If % by 3 print fizz, if % by 5 print buzz, if % by 3&5 print # (push to fizzBuzzArray)
+numArray.map((x) => {
+  // Use a conditional to check division
+  if (x % 15 === 0){
+    fizzBuzzArray.push(x);
+  } else if(x % 3 === 0) {
+    fizzBuzzArray.push("Fizz");
+} else if (x % 5 === 0){
+  fizzBuzzArray.push("Buzz");
+}
+})
+
+// Check if fizzBuzzArray populated
+console.log( "FizzBuzzArray:",fizzBuzzArray);
+
+// ? ---------- map vs forEach example -----------
+
+let mainArr = [1, 2, 3, 4, ];
+// .forEach does NOT return anything, perfors an action on each item, does not retun a value
+let forEachex = mainArr.forEach((i) => i);
+console.log("ForEach Ex: ", forEachex);
+
+// .map returns a new array, perform action & creates/returns new value
+let mapEx = mainArr.map((i) => i + 3);
+console.log( "Map Ex:",mapEx);
+
+let secondMap = mapEx.map((i) => i + 5);
+console.log("SecondMap Ex:", secondMap);
+console.log(mainArr)
+
+// -------------------------------------------
+
+//? .find()
+
+let tmnt =  ["Mikey", "Donnie", "Leo", "Raph", "Splinter", "Shredder", "Baxter"];
+
+let character = "Leo";
+
+console.log(
+  "Find Ex 1: ",
+  tmnt.find((c) => c === character)
+);
+
+character = "April";
+console.log(
+  "Find Ex 2: ",
+  tmnt.find((c) => c === character)
+);
+
+character = "Splinter"; // find will return true for splinter vs the value splinter  because we checked all items via including the index
+console.log(
+  "Find Ex 3: ",
+  tmnt.find((c, i) => console.log(
+    "Character:",
+    c === character,
+    "Index: ",
+    i
+  ))
+);
+
+/* 
+  - Executes a function for each array element and returns the value of the first element that passes a test.
+  - Returns undefined if no elements are found.
+  - Does not execute the function for empty elements.
+  - Does not change the original array.
+*/
